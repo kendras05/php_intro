72187cd12dcd673a5cd000d1c005acec026c0d24
user <user01@KMS-MacBook.local>
2019-12-02 20:58:23 -0800

    added function to determine if word is more or less than 5 letters with if loop

--- wordsgame.php
+++ wordsgame.php
@@ -4,21 +4,25 @@ function count_letter($input) {
 	return strlen($input);
 }
 
-
+function letter_game($input) {
+	if ($lettercount > 5){
+		return strrev(substr($input, 0, 4));
+} else {
+		return $input;
+}
+}
 
 
 echo "Please type a word: ";
 $input = rtrim(fgets(STDIN)); 
 $userword = $input;
 $lettercount = count_letter($input);
-
+$lettergame = letter_game($input);
 
 //echo $userword;
 //echo $lettercount;
 
-echo "Your word is {$userword} and the length of the letters are {$lettercount};"
+echo "Your word is {$userword} and the length of the letters are {$lettercount}\n";
 
-// echo "{$message}\n";
-
-?>
+echo $lettergame; 
 
