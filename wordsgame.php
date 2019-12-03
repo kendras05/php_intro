aabea09d427f1a4574ac8202ec9c76c4f8c36e3d
user <user01@KMS-MacBook.local>
2019-12-02 20:27:12 -0800

    successfully output user input and letter count

--- wordsgame.php
+++ wordsgame.php
@@ -1,12 +1,24 @@
 <?php
 
+function count_letter($input) {
+	return strlen($input);
+}
+
+
 
 
 echo "Please type a word: ";
 $input = rtrim(fgets(STDIN)); 
 $userword = $input;
+$lettercount = count_letter($input);
 
-echo $userword;
+
+//echo $userword;
+//echo $lettercount;
+
+echo "Your word is {$userword} and the length of the letters are {$lettercount};"
+
+// echo "{$message}\n";
 
 ?>
 
